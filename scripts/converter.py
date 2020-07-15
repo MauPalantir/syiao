a1fa48e0914abba2f4714216ff0af80eb19996f9
Zsofia Abraham <mau.palantir@gmail.com>
2020-07-15 18:37:19 +0200

    Lots of sounds, IPA, split senses

--- scripts/converter.py
+++ scripts/converter.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import csv
+import os
 import re
 from lxml import etree
 from xml.sax.saxutils import escape
 import click
-
+from unidecode import unidecode
+from subprocess import check_output
 
 class TEI:
     def __init__(self, data):
@@ -18,26 +20,43 @@ class TEI:
         extent.attrib['quantity'] = str(len(self.data))
         extent.text = "{} words".format(len(self.data))
 
+    def word2id(self, word):
+        return re.sub(r'\W', '', unidecode(word).replace('@', 'e').replace(' ', '_'))
+
+    def pron(self, word, wid):
+        path = os.path.dirname(os.path.realpath(__file__))
+        out = check_output(['{}/../../espeak-ng/src/espeak-ng --ipa -v sme -w "{}/../wav/{}.wav" "{}"'.format(path, path, wid, word)], shell=True).decode('UTF-8').strip()
+        return out.replace('y', 'ɯ')
+
+
     def generate(self):
+        n = 0
         for row in self.data:
-            entry = etree.Element('entry')
+            n = n+1
+            word = row[0]
+            pos_class = row[1]
+            wid = '__'.join([self.word2id(word), pos_class, str(n)])
+
+            entry = etree.Element('entry', attrib={"{http://www.w3.org/XML/1998/namespace}id": wid})
 
             form = etree.SubElement(entry, 'form', type='lemma')
             orth = etree.SubElement(form, 'orth')
-            orth.text = row[0]
+            orth.text = word
 
             gramgrp = etree.SubElement(entry, 'gramGrp')
             pos = etree.SubElement(gramgrp, 'pos')
-            pos.text = row[1]
+            pos.text = pos_class
+            p=self.pron(re.sub(r'[^\w\s]', '', word), wid)
+            pron = etree.SubElement(form, 'pron', notation="ipa")
+            pron.text = p
 
-            sense = etree.SubElement(entry, 'sense')
             for meaning in row[2].split(';'):
+                sense = etree.SubElement(entry, 'sense')
                 cit = etree.SubElement(sense, 'cit', type='translation', attrib={"{http://www.w3.org/XML/1998/namespace}lang": "hu"})
                 translation = etree.SubElement(cit, 'quote')
-                translation.text = meaning
+                translation.text = meaning.strip()
 
             if row[3] != '' or row[5] != '':
-
                 if row[3] != '': 
                     etym = etree.SubElement(entry, 'etym', type="root")
                     etym_lang = etree.SubElement(etym, 'lang')
@@ -60,8 +79,8 @@ class TEI:
                         etym_form.text = vals[2]
                 if row[6] != '':
                     glossval = re.sub(r'([A-Z]+) ([\w\s]+\w)', '<lang>\\1</lang> <mentioned>\\2</mentioned>', escape(row[6]))
-                    str = '<gloss>{0}</gloss>'.format(glossval)
-                    glossval_elem = etree.fromstring(str)
+                    string = '<gloss>{0}</gloss>'.format(glossval)
+                    glossval_elem = etree.fromstring(string)
                     etym.append(glossval_elem)
 
             self.root.xpath('//t:body', namespaces=self.ns)[0].append(entry)
