981e9a15cf3ac887cbf6512326f3190298965dae
Zsofia Abraham <mau.palantir@gmail.com>
2020-08-08 18:43:40 +0200

    A few english fixes

--- refresh-db.sh
+++ refresh-db.sh
@@ -1,1 +1,1 @@
-curl -i -X PUT -T /home/zabraham/Development/syiao/tei/syiao-mei.xml "https://admin:${SYIAO_ADMIN_PASS}@syiao-mei.hu/rest/syiao"
+curl -i -X PUT -T /home/zabraham/Development/syiao/tei/syiao-mei.xml "https://admin:${SYIAO_ADMIN_PASS}@${SYIAO_HOST}/rest/syiao"
---
+++ refresh-local-db.sh
@@ -0,0 +1,1 @@
+curl -i -X PUT -T /home/zabraham/Development/syiao/tei/syiao-mei.xml "http://admin:admin@localhost/rest/syiao"
